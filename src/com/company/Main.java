ebff1862835feeaacd862ab3fb14d57f8654bf42
Balan-Claudiu <balan_claudiu90@yahoo.com>
2020-04-12 22:41:16 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Claudiu");
+
+        int x = 5;
+        int y = 10;
+        System.out.println(x+y);
+
+        int a = 27;
+        int b = 3;
+        System.out.println(a/b);;
+
+        int c = -5;
+        int d = 8;
+        int e = 6;
+        System.out.println(c + d *e);
+
+        int f = 55;
+        int g = 9;
+        System.out.println((f+g)%g);
+
+        int h = 15;
+        int i = 2;
+        int j = 8;
+        System.out.println(x + h / b * i - j % b);
+
+
+
+
+
+	// write your code here
+    }
+}
