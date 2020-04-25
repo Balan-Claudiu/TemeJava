79ce9f158e2f2978cd781dc523630f926fe55852
Balan-Claudiu <balan_claudiu90@yahoo.com>
2020-04-25 21:21:25 +0300

    Tema 2

---
+++ src/com/company/Lab2.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Lab2 {
+
+    public static void main(String[] args) {
+    printMyName();
+
+    int adunare = sum(8,2);
+        System.out.println(adunare);
+
+    int scadere = substraction(10,3);
+        System.out.println(scadere);
+
+    int inmultire = multiplication(9,5);
+        System.out.println(inmultire);
+
+    int impartire = division(55,11);
+        System.out.println(impartire);
+
+    }
+    public static void printMyName(){
+        System.out.println("Hello \n Claudiu");
+    }
+
+    public static int sum( int x,  int y){
+        int sum = x+y;
+        return sum;
+}
+    public static int substraction(int x, int y){
+        int substraction = x-y;
+        return substraction;
+    }
+    public static int multiplication(int x, int y){
+        int multiplication = x*y;
+        return multiplication;
+
+    }
+    public static int division( int x, int y){
+        int division = x / y;
+        return division;
+    }
+   }
+
+
+
