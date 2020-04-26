4cf57d9047f88370f76a7d16cdb99ed9f1f27d8f
Balan-Claudiu <balan_claudiu90@yahoo.com>
2020-04-26 19:10:50 +0300

    Tema 2

--- src/com/company/Lab2.java
+++ src/com/company/Lab2.java
@@ -20,6 +20,23 @@ public class Lab2 {
     int media = average(10, 15, 20 ,3);
         System.out.println(media);
 
+    int restulImpartirii = rest (8,3);
+        System.out.println(restulImpartirii);
+
+     int fahrenheit = celsius(80);
+        System.out.println(fahrenheit);
+
+     float cm = inch(10);
+        System.out.println(cm);
+
+    int metripesecunda = ms(100000 , 1,60,60);
+        System.out.println(metripesecunda);
+
+    int kmpeora = kmora(75, 1);
+        System.out.println(kmpeora);
+
+    int milepeora = mpeora(10);
+        System.out.println(milepeora);
     }
     public static void printMyName(){
         System.out.println("Hello \n Claudiu");
@@ -46,6 +63,34 @@ public class Lab2 {
     public static int average(int x, int y , int z, int a){
         int average = (x+y+z)/a;
         return average;
+    }
+
+    public static int rest(int x, int y){
+        int rest= x % y;
+        return rest;
+    }
+    public static int celsius(int f){
+        int celsius = (f-32)*5/9;
+        return celsius;
+    }
+    public static float inch(float x){
+        float inch = 2.54f * x;
+        return inch;
+    }
+
+    public static int ms(int a, int b, int c, int d){
+        int ms= a / (b*c*d);
+        return ms;
+
+
+    }
+    public static int kmora(int a, int b){
+        int kmora = a / b;
+        return kmora;
+    }
+    public static int mpeora(int a){
+        int mpeora = a * 1609;
+        return mpeora;
     }
 
     }
