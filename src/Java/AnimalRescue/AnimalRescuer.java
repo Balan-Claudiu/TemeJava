24b5d2e50e2dae1e184a951dc3f2d991a0a0ba2c
Balan-Claudiu <balan_claudiu90@yahoo.com>
2020-05-13 12:03:54 +0300

    Tema 4

---
+++ src/Java/AnimalRescue/AnimalRescuer.java
@@ -0,0 +1,23 @@
+package Java.AnimalRescue;
+
+public class AnimalRescuer {
+
+    private String name;
+    private int bani;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBani() {
+        return bani;
+    }
+
+    public void setBani(int bani) {
+        this.bani = bani;
+    }
+}
